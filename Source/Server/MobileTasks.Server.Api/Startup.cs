348e84f608b18adb43bf06aa55c7f2364f8e3e14
Brent Edwards <brentledwards@live.com>
2016-01-12 16:56:32 -0600

    Adding to source control.

---
+++ Source/Server/MobileTasks.Server.Api/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MobileTasks.Server.Api.Startup))]
+
+namespace MobileTasks.Server.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
