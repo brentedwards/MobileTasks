ef05c3656d9903eabcff780f9ee7ff4b79d31350
Brent Edwards <brentledwards@live.com>
2016-04-28 14:14:44 -0500

    Adding initial XForms solution.

---
+++ Source/Client/Xamarin.Forms/MobileTasks.XForms/MobileTasks.XForms/MobileTasks.XForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileTasks.XForms.ViewModels
+{
+    public class LoginViewModel
+    {
+    }
+}
---
+++ Source/Client/Xamarin.Forms/MobileTasks.XForms/MobileTasks.XForms/MobileTasks.XForms/Views/Login.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MobileTasks.XForms.Views
+{
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
