d38628fef345277fcc3b2cbd6a4646f61cb11bd3
Kevin E. Ford <bowman74@hotmail.com>
2016-05-09 12:42:58 -0400

    Styling and finishing changes to Swift Project

---
+++ Source/Client/NativeIos/MobileTasks.iOS/malmobileapp/TaskProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  TaskDelegate.swift
+//  MobileTasks.iOS
+//
+//  Created by kevin Ford on 5/4/16.
+//  Copyright © 2016 Microsoft. All rights reserved.
+//
+
+import Foundation
+
+protocol TaskProtocol {
+    func didSaveItem(task : MobileTask)
+}
