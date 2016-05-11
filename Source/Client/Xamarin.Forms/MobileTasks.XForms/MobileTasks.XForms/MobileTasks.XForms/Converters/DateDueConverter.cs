53533d28e05b53a3adb83376db4dcfbe8434533f
Brent Edwards <brentledwards@live.com>
2016-05-11 11:09:47 -0500

    Updating some styling for login and tasks pages in XForms.

---
+++ Source/Client/Xamarin.Forms/MobileTasks.XForms/MobileTasks.XForms/MobileTasks.XForms/Converters/DateDueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MobileTasks.XForms.Converters
+{
+	public class DateDueConverter : IValueConverter
+	{
+		public string NoDateDueText { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value != null && value is DateTime)
+			{
+				return ((DateTime)value).ToString("ddd, M/d/yy h:mm tt");
+			}
+
+			return this.NoDateDueText;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
