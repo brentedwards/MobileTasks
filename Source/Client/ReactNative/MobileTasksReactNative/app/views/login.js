00be7f151aeff97e08636f8dc5eee74aecdf4975
Kevin E. Ford <bowman74@hotmail.com>
2017-10-02 16:29:09 -0400

    With Auth0 initially working.

--- Source/Client/ReactNative/MobileTasksReactNative/app/views/login.js
+++ Source/Client/ReactNative/MobileTasksReactNative/app/views/login.js
@@ -18,23 +18,30 @@ import {
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import FlexImage from 'react-native-flex-image';
+import Auth0 from 'react-native-auth0';
 
 export default class Login extends Component {
     _onMicrosoftTapped() {
-        Alert.alert('You tapped the button!')
+        //Alert.alert('You tapped the button!')
+        var auth0 = new Auth0({ domain: 'vslivemobiletasks.auth0.com', clientId: '9Z4fFNtg66Z1Yy2KWOOgMlLIRP60lIEn' });
+        auth0
+        .webAuth
+        .authorize({scope: 'openid email', audience: 'https://vslivemobiletasks.auth0.com/userinfo'})
+        .then(credentials => Alert.alert(credentials.accessToken))
+        .catch(error => Alert.alert(error.error));
       }
   render() {
     return (
         <LinearGradient colors={['#B4EC51', '#429321']} style={{flex: 1, flexDirection: 'column', paddingTop: 18}} >
             <View style={{flex: .3}} />
-            <View style={{flex: .75, backgroundColor: 'red', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingLeft: 20}} >
+            <View style={{flex: .75, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', paddingLeft: 20}} >
                 <FlexImage source={require('../images/Logo.png')} style={{flex: .75}} />
             </View>
-            <View style={{flex: .05, backgroundColor: 'blue'}} />
-           <View style={{flex: .33, backgroundColor: 'red', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{flex: .05}} />
+           <View style={{flex: .33, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
                <Text style={styles.loginLabel}>Login</Text>
            </View>
-           <View style={{flexDirection: 'row', backgroundColor: 'blue', justifyContent: 'space-between', alignItems: 'center'}}>
+           <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
                 <View style={{width: 0}} />
                 <TouchableOpacity style={{flex: .2}} onPress={this._onMicrosoftTapped}>
                     <FlexImage source={require('../images/IconTwitter.png')} />
@@ -50,7 +57,7 @@ export default class Login extends Component {
                 </TouchableOpacity>
                 <View style={{width: 0}} />
            </View>
-           <View style={{flexDirection: 'row', backgroundColor: 'red', justifyContent: 'space-between'}}>
+           <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                 <View style={{width: 0}} />
                 <Text style={styles.buttonLabel}>Twitter</Text>
                 <Text style={styles.buttonLabel}>Facebook</Text>
@@ -58,7 +65,7 @@ export default class Login extends Component {
                 <Text style={styles.buttonLabel}>Google</Text>
                 <View style={{width: 0}} />
            </View>
-           <View style={{flex: .2, backgroundColor: 'blue'}} />
+           <View style={{flex: .2}} />
       </LinearGradient>
     );
   }
