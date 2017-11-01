ba584bc4cf8552fb89526453322c9cc5be1f66e9
Kevin E. Ford <bowman74@hotmail.com>
2017-11-01 11:59:02 -0400

    Rebuilt Android project to get it working again.

--- Source/Client/ReactNative/MobileTasksReactNative/android/app/src/main/java/com/mobiletasksreactnative/MainApplication.java
+++ Source/Client/ReactNative/MobileTasksReactNative/android/app/src/main/java/com/mobiletasksreactnative/MainApplication.java
@@ -3,8 +3,6 @@ package com.mobiletasksreactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.auth0.react.A0Auth0Package;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new A0Auth0Package(),
-            new LinearGradientPackage()
+          new MainReactPackage()
       );
     }
   };
